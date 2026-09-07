63b94c1b9b2edc4a349557d90adfc0effd146c5e
DaNa-cpu <grancea9d@gmail.com>
2026-09-07 20:26:32 +0300

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    unordered_map<int, int> memo;
+public:
+    int climbStairs(int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(memo.count(n)) return memo[n];
+        return memo[n] = climbStairs(n-1) + climbStairs(n-2);
+    }
+};
