3812122c2685b93c74af702487bea604bf52bb82
DaNa-cpu <grancea9d@gmail.com>
2026-09-10 21:37:42 +0300

    Add: balanced-binary-tree - submission-8

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-8.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    bool balanced = true;
+public:
+    bool isBalanced(TreeNode* root) {
+        height(root);
+        return balanced;
+    }
+private:
+    int height(TreeNode* root){
+        if(!root){
+            return 0;
+        }
+        int left = height(root->left);
+        
+        int right = height(root->right);
+        if(abs(left - right) > 1){
+            balanced = false;
+            return 0;
+        }
+        return 1 + max(left, right);
+    }
+};
