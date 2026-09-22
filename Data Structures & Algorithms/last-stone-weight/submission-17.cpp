18ad7eb23805af719990352b34ba11f6ef101edb
DaNa-cpu <grancea9d@gmail.com>
2026-09-22 13:32:20 +0300

    Add: last-stone-weight - submission-17

---
+++ Data Structures & Algorithms/last-stone-weight/submission-17.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for( auto& stone: stones){
+            maxHeap.push(stone);
+        }
+        while(maxHeap.size() > 1){
+            int max1 = maxHeap.top();
+            maxHeap.pop();
+            int max2 = maxHeap.top();
+            maxHeap.pop();
+            maxHeap.push(max1 - max2);
+
+        }
+        maxHeap.push(0);
+        return maxHeap.top();
+
+    }
+};
